428586a1445aa7167048363a5e07358ca51b486e
Niraj Limbu <nlimbu@yahoo.com>
2022-02-09 16:39:52 -0500

    create authguard routes

--- controllers/post-routers.js
+++ controllers/post-routers.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connections');
 const { Post, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
   console.log('======================');
@@ -62,7 +63,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
@@ -75,7 +76,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -99,7 +100,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
   console.log('id', req.params.id);
   Post.destroy({
     where: {
